4c85c56c247730d4dfde902d297d541949edccf9
ChristianEllerbrock <christian.ellerbrock@inbayern.net>
2020-05-09 01:31:06 +0200

    2020.05.09

--- app/components/new-view.ts
+++ app/components/new-view.ts
@@ -1,5 +1,40 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { CreateTenantInput } from 'ember-appo-admin/gql/types';
 
-interface NewViewArgs {}
+interface NewViewArgs {
+  createTenant(input: CreateTenantInput): Promise<void>;
+}
 
-export default class NewView extends Component<NewViewArgs> {}
+export default class NewView extends Component<NewViewArgs> {
+  @tracked name: string = '';
+  @tracked adminEmail: string = '';
+  @tracked dbName: string = '';
+
+  get canSave() {
+    if (this.name && this.adminEmail && this.dbName) {
+      return true;
+    }
+    return false;
+  }
+
+  @action
+  async save() {
+    if (!this.args.createTenant || !this.canSave) {
+      return;
+    }
+
+    const input: CreateTenantInput = {
+      name: this.name,
+      adminEmail: this.adminEmail,
+      dbName: this.dbName,
+    };
+
+    try {
+      await this.args.createTenant(input);
+    } catch (error) {
+      // error handling
+    }
+  }
+}
---
+++ app/controllers/protected/new.ts
@@ -0,0 +1,27 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { CreateTenantInput } from 'ember-appo-admin/gql/types';
+import createTenantMutation from 'ember-appo-admin/gql/mutations/create-tenant.graphql';
+
+export default class ProtectedNew extends Controller.extend({
+  // anything which *must* be merged to prototype here
+}) {
+  @service apollo: any;
+
+  @action
+  createTenant(input: CreateTenantInput) {
+    let variables = {
+      input: input,
+    };
+
+    return this.apollo.mutate({ mutation: createTenantMutation, variables }, 'tenant');
+  }
+}
+
+// DO NOT DELETE: this is how TypeScript knows how to look up your controllers.
+declare module '@ember/controller' {
+  interface Registry {
+    'protected/new': ProtectedNew;
+  }
+}
