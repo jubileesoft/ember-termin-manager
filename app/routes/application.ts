ad68e95d44ac31a5ad2b887934b7a61a8e81ec92
ChristianEllerbrock <christian.ellerbrock@inbayern.net>
2020-05-14 01:00:10 +0200

    2020.05.14

--- app/routes/application.ts
+++ app/routes/application.ts
@@ -1,9 +1,16 @@
 import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import { inject as service } from '@ember/service';
 
 export default class Application extends Route.extend(ApplicationRouteMixin, {
   // anything which *must* be merged to prototype here
   routeAfterAuthentication: 'protected'
 }) {
+  @service intl: any;
+
   // normal class body definition here
+  constructor() {
+    super(...arguments);
+    this.intl.locale = 'de';
+  }
 }
--- app/routes/protected/admin.ts
+++ app/routes/protected/admin.ts
@@ -1,7 +1,13 @@
 import Route from '@ember/routing/route';
+import { queryManager } from 'ember-apollo-client';
+import GetAdminDatabaseInfoQuery from 'ember-termin-manager/gql/queries/get-admin-database-info.graphql';
 
 export default class ProtectedAdmin extends Route.extend({
   // anything which *must* be merged to prototype here
 }) {
-  // normal class body definition here
+  @queryManager apollo: any;
+
+  model() {
+    return this.apollo.query({query: GetAdminDatabaseInfoQuery}, 'getAdminDatabaseInfo');
+  }
 }
